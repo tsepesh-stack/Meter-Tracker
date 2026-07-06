08dee09e3060f4b9a1ecb054716668469d851464
tsepesh-stack <tsepeshww@gmail.com>
2026-07-06 23:20:29 +0300

    feat: register services in Program.cs

--- MeterTrackerApi/Program.cs
+++ MeterTrackerApi/Program.cs
@@ -1,3 +1,5 @@
+using MeterTrackerApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<MeterService>();
+builder.Services.AddSingleton<PremiseService>();
+builder.Services.AddSingleton<ReadingService>();
 
 var app = builder.Build();
 
