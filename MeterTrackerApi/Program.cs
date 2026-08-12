37676c1f50cae1352be27fca77acaff0fb2e1892
tsepesh-stack <tsepeshww@gmail.com>
2026-08-13 01:50:45 +0300

    Добавил CORS origin для продакшен-фронта на VPS

--- MeterTrackerApi/Program.cs
+++ MeterTrackerApi/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5501")
+        policy.WithOrigins("http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://159.194.205.124")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
